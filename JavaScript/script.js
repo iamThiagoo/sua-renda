6956f831f43e4f5f92f9677be2afcc5e8b6f5296
iamThiagoo <thiagofer2003@outlook.com>
2021-06-17 18:25:20 -0300

    Melhora no código JavaScript e iniciando o processo de apresentação do resultado.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,37 +1,27 @@
-/*Iniciando minha variáveis*/
-let rendaPorMes;
-let aguaPorMes;
-let luzPorMes;
-let mercadoPorMes;
-let habitacaoPorMes;
-let wifiPorMes;
-let televisaoPorMes;
-let telefonePorMes;
-let saudePorMes;
-let impostoPorMes;
-let veiculoPorMes;
-let despesasPessoaisPorMes;
-let lazerPorMes;
-
 function resultado(){
-    rendaPorMes = document.getElementById("rendaMensal").value;
-    aguaPorMes = document.getElementById("aguaMensal").value;
-    luzPorMes = document.getElementById("energiaMensal").value;
-    mercadoPorMes = document.getElementById("mercadoMensal").value;
-    habitacaoPorMes = document.getElementById("habitacaoMensal").value;
-    wifiPorMes = document.getElementById("wifiMensal").value;
-    televisaoPorMes = document.getElementById("TVMensal").value;
-    telefonePorMes = document.getElementById("telefoneMensal").value;
-    saudePorMes = document.getElementById("saudeMensal").value;
-    impostoPorMes = document.getElementById("impostoMensal").value;
-    veiculoPorMes = document.getElementById("automovelMensal").value;
-    despesasPessoaisPorMes = document.getElementById("despesasPessoaisMensal").value;
-    lazerPorMes = document.getElementById("lazerMensal").value;
-    console.log(`Seu salário é de R$${rendaPorMes} por mês.`);
-    let somaDosGastosMensal = Number(aguaPorMes) + Number(luzPorMes); 
-    let lucroMensal = Number(rendaPorMes) - somaDosGastosMensal;
-    console.log(`Seu gasto total durante o mês é de R$${somaDosGastosMensal} e seu lucro é de R$${lucroMensal} reais ao mês.`);
-    console.log(typeof(somaDosGastosMensal));
-    console.log(typeof(lucroMensal));
+    let rendaPorMes = document.getElementById("rendaMensal").value;
+    let aguaPorMes = document.getElementById("aguaMensal").value;
+    let luzPorMes = document.getElementById("energiaMensal").value;
+    let mercadoPorMes = document.getElementById("mercadoMensal").value;
+    let habitacaoPorMes = document.getElementById("habitacaoMensal").value;
+    let internetPorMes = document.getElementById("internetMensal").value;
+    let televisaoPorMes = document.getElementById("TVMensal").value;
+    let telefonePorMes = document.getElementById("telefoneMensal").value;
+    let saudePorMes = document.getElementById("saudeMensal").value;
+    let impostoPorMes = document.getElementById("impostoMensal").value;
+    let veiculoPorMes = document.getElementById("automovelMensal").value;
+    let despesasPessoaisPorMes = document.getElementById("despesasPessoaisMensal").value;
+    let lazerPorMes = document.getElementById("lazerMensal").value;
+
+    let somaDosGastosMensal = Number(aguaPorMes) + Number(luzPorMes) + Number(mercadoPorMes) + Number(habitacaoPorMes) + Number(internetPorMes) + Number(televisaoPorMes) + Number(telefonePorMes) + Number(saudePorMes) + Number(impostoPorMes) + Number(veiculoPorMes) + Number(despesasPessoaisPorMes) + Number(lazerPorMes); 
+    let lucroMensal = Number(rendaPorMes) - Number(somaDosGastosMensal);
+
+    let resultadoMensal = document.querySelector("#saldoDoMes");
+    
+    if(lucroMensal > 0 ){
+        resultadoMensal.innerHTML = `Seu mês fecha feliz com um saldo positivo de ${lucroMensal} reais 😁`;
+    } else{
+        resultadoMensal.innerHTML = `Seu mês fecha triste com um saldo negativo de ${lucroMensal} reais 😰`;
+    }
 }
 

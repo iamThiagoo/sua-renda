db3afa097ba1a8b444a5695b65450bc97cbf3aaf
iamThiagoo <thiagofer2003@outlook.com>
2021-06-19 23:18:11 -0300

    Término da FUNCIONALIDADE e mudanças no LAYOUT.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -19,13 +19,13 @@ function resultado(){
     let resultadoMensal = document.querySelector("#saldoDoMes");
     
     if(lucroMensal > 0 ){
-        resultadoMensal.innerText = `Seu mês fecha feliz com um SALDO POSITIVO de  ${lucroMensal} reais.`;
+        resultadoMensal.innerText = `Seu mês fecha ANIMADO com um SALDO POSITIVO de  ${lucroMensal} reais.`;
     } 
     else if(lucroMensal == 0){
         resultadoMensal.innerText = `Seu mês fecha ZERADO!`;
     }
     else{
-        resultadoMensal.innerText = `Seu mês fecha bem triste com um SALDO NEGATIVO de  ${lucroMensal} reais.`;
+        resultadoMensal.innerText = `Seu mês fecha TRISTE com um SALDO NEGATIVO de  ${lucroMensal} reais.`;
     }
 
     let somaMensal = document.querySelector("#somaDosGastosDoMes");
@@ -38,97 +38,133 @@ function resultado(){
     apareceContainerAoClicar.style.display = "flex";
     apareceContainerAoClicar.style.justifyContent = "space-around";
 
+    // Início do relatório   
     let containerResultadoAnual = document.querySelector("#container-anual");
     containerResultadoAnual.style.display = "flex";
     containerResultadoAnual.style.justifyContent = "center";
     containerResultadoAnual.style.alignItems = "center";
+    containerResultadoAnual.style.flexDirection = "column";
     
     let aguaAnual = Number(aguaPorMes) * 12;
         let textoAguaAnual = document.querySelector("#aguaPorAno");
-            textoAguaAnual.innerText = `Gasto com ÁGUA: R$ ${aguaAnual}`;    
+            textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: R$ ${aguaAnual}`;    
             if(aguaAnual == 0){
-                textoAguaAnual.innerText = `Gasto com ÁGUA: sem gasto ou não preenchido.`;
+                textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: sem gasto ou não preenchido.`;
             }        
 
     let luzAnual = Number(luzPorMes) * 12;
         let textoLuzAnual = document.querySelector("#luzPorAno");
-            textoLuzAnual.innerText = `Gasto com ENERGIA: R$ ${luzAnual}`;
+            textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: R$ ${luzAnual}`;
             if(luzAnual == 0){
-                textoLuzAnual.innerText = `Gasto com ENERGIA: sem gasto ou não preenchido.`;
+                textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: sem gasto ou não preenchido.`;
             }     
 
     let mercadoAnual = Number(mercadoPorMes) * 12;
         let textoMercadoAnual = document.querySelector("#mercadoPorAno");
-            textoMercadoAnual.innerText = `Gasto com MERCADO: R$ ${mercadoAnual}`;
+            textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: R$ ${mercadoAnual}`;
             if(mercadoAnual == 0){ 
-                textoMercadoAnual.innerText = `Gasto com MERCADO: sem gasto ou não preenchido.`;
+                textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: sem gasto ou não preenchido.`;
             }     
 
     let habitacaoAnual = Number(habitacaoPorMes) * 12;
         let textoHabitacaoAnual = document.querySelector("#habitacaoPorAno");
-            textoHabitacaoAnual.innerText = `Gasto com HABITAÇÃO: R$ ${habitacaoAnual}`;
+            textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: R$ ${habitacaoAnual}`;
             if(habitacaoAnual == 0){
-                textoHabitacaoAnual.innerText = `Gasto com HABITAÇÃO: sem gasto ou não preenchido.`;
+                textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: sem gasto ou não preenchido.`;
             }     
 
     let internetAnual = Number(internetPorMes) * 12;
         let textoInternetAnual = document.querySelector("#internetPorAno");
-            textoInternetAnual.innerText = `Gasto com INTERNET: R$ ${internetAnual}`;
+            textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: R$ ${internetAnual}`;
             if(internetAnual == 0){
-                textoInternetAnual.innerText = `Gasto com INTERNET: sem gasto ou não preenchido.`;
+                textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: sem gasto ou não preenchido.`;
             }     
 
     let televisaoAnual = Number(televisaoPorMes) * 12;
         let textoTelevisaoAnual = document.querySelector("#televisaoPorAno");
-            textoTelevisaoAnual.innerText = `Gasto com TV: R$ ${televisaoAnual}`;
+            textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: R$ ${televisaoAnual}`;
             if(televisaoAnual == 0){
-                textoTelevisaoAnual.innerText = `Gasto com TV: sem gasto ou não preenchido.`;
+                textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: sem gasto ou não preenchido.`;
             }     
 
     let telefoneAnual = Number(telefonePorMes) * 12;
         let textoTelefoneAnual = document.querySelector("#telefonePorAno");
-            textoTelefoneAnual.innerText = `Gasto com TELEFONE: R$ ${telefoneAnual}`;
+            textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: R$ ${telefoneAnual}`;
             if(telefoneAnual == 0){
-                textoTelefoneAnual.innerText = `Gasto com TELEFONE: sem gasto ou não preenchido.`;
+                textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: sem gasto ou não preenchido.`;
             }     
 
     let saudeAnual = Number(saudePorMes) * 12;
         let textoSaudeAnual = document.querySelector("#saudePorAno");
-            textoSaudeAnual.innerText = `Gasto com SAÚDE: R$ ${saudeAnual}`;
+            textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: R$ ${saudeAnual}`;
             if(saudeAnual == 0){
-                textoSaudeAnual.innerText = `Gasto com SAÚDE: sem gasto ou não preenchido.`;
+                textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: sem gasto ou não preenchido.`;
             }     
 
     let impostoAnual = Number(impostoPorMes) * 12;
         let textoImpostoAnual = document.querySelector("#impostoPorAno");
-            textoImpostoAnual.innerText = `Gasto com IMPOSTO: R$ ${impostoAnual}`;
+            textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: R$ ${impostoAnual}`;
             if(impostoAnual == 0){
-                textoImpostoAnual.innerText = `Gasto com IMPOSTO: sem gasto ou não preenchido.`;
+                textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: sem gasto ou não preenchido.`;
             }     
 
     let veiculoAnual = Number(veiculoPorMes) * 12;
         let textoVeiculoAnual = document.querySelector("#veiculoPorAno");
-            textoVeiculoAnual.innerText = `Gasto com AUTOMÓVEL: R$ ${veiculoAnual}`;
+            textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: R$ ${veiculoAnual}`;
             if(veiculoAnual == 0){
-                textoVeiculoAnual.innerText = `Gasto com AUTOMÓVEL: sem gasto ou não preenchido.`;
+                textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: sem gasto ou não preenchido.`;
             }     
 
     let despesasPessoaisAnual = Number(despesasPessoaisPorMes) * 12;
         let textoDespesasAnual = document.querySelector("#despesasPessoaisPorAno");
-            textoDespesasAnual.innerText = `Gasto com DESPESAS PESSOAIS: R$ ${despesasPessoaisAnual}`;
+            textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: R$ ${despesasPessoaisAnual}`;
             if(despesasPessoaisAnual == 0){
-                textoDespesasAnual.innerText = `Gasto com DESPESAS PESSOAIS: sem gasto ou não preenchido.`;
+                textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: sem gasto ou não preenchido.`;
             }     
     
     let lazerAnual = Number(lazerPorMes) * 12;
         let textoLazerAnual = document.querySelector("#lazerPorAno");
-            textoLazerAnual.innerText = `Gasto com LAZER: R$ ${lazerAnual}`;
+            textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: R$ ${lazerAnual}`;
             if(lazerAnual == 0){
-                textoLazerAnual.innerText = `Gasto com LAZER: sem gasto ou não preenchido.`;
+                textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: sem gasto ou não preenchido.`;
             }     
 
+    // 2°Parte do relatório
     let rendaAnual = Number(rendaPorMes) * 12;
+        let textoRendaAnual = document.querySelector("#rendaAnual");
+            textoRendaAnual.innerHTML = `Sua <b>RENDA</b> anual gira em torno de R$ ${rendaAnual}`;
+            if(rendaAnual > 0){
+                textoRendaAnual.style.color = "#0C9FCA";
+            }
+            else{
+                textoRendaAnual.innerText = `Renda anual: [ERRO] Preencha os dados para ver o resultado!`;
+                textoLucroAnual.style.fontColor = "#F00D25";
+            }
+            
     let somaDosGastosAnual = Number(somaDosGastosMensal) * 12;
-    let lucroAnual = rendaAnual - somaDosGastosAnual;
-}
+        let textoSomaAnual = document.querySelector("#somaDosGastosAnual");
+            textoSomaAnual.innerHTML = `Seu <b>GASTO</b> anual gira em torno de R$ ${somaDosGastosAnual}`;
+            textoSomaAnual.style.color = "#D80D22"; 
+            if(somaDosGastosAnual == 0){
+                textoSomaAnual.innerText = `Gasto anual: [ERRO] Preencha os dados para ver o resultado!`;
+            }
 
+    let lucroAnual = rendaAnual - somaDosGastosAnual;
+        let textoLucroAnual = document.querySelector("#lucroAnual");
+            textoLucroAnual.innerHTML = `Seu <b>LUCRO</b> anual gira em torno de R$ ${lucroAnual}`;
+            if(lucroAnual > 0){
+                textoLucroAnual.style.color = "#19D327";
+            }
+            else if(lucroAnual == 0){
+                textoLucroAnual.innerText = `Seu lucro anual é IGUAL a 0 ou você NÃO preencheu sua renda ou seus gastos`;
+                textoLucroAnual.style.color = "#0DBAEC";
+            }
+            else{
+                textoLucroAnual.style.color = "#D80D22";
+            }
+} 
+
+function misterio(){
+    alert(`Parece que você encontrou meu enigma!!!
+Na verdade não é nada demais kkk só quero te desejar um bom dia e que tudo na sua vida dê certo 😊`);
+}

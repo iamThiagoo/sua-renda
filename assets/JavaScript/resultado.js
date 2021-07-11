c7d45a5e05228f53130ad30aae666d36b15afed7
iamThiagoo <thiagofer2003@outlook.com>
2021-07-11 01:08:45 -0300

    Finalização da funcionalidade

--- assets/JavaScript/resultado.js
+++ assets/JavaScript/resultado.js
@@ -1,6 +1,9 @@
 function resultados(){
+
+    function mostraContainer(){
     let containerResultado = document.querySelector("#div-resultado");
-        containerResultado.style.display = "block";
+    containerResultado.style.display = "block";
+    } mostraContainer();
 
     let rendaPorMes = document.getElementById("rendaMensal").value;
     let aguaPorMes = document.getElementById("aguaMensal").value;
@@ -16,171 +19,137 @@ function resultados(){
     let despesasPessoaisPorMes = document.getElementById("despesasPessoaisMensal").value;
     let lazerPorMes = document.getElementById("lazerMensal").value;
 
-    let somaDosGastosMensal = Number(aguaPorMes) + Number(luzPorMes) + Number(mercadoPorMes) + Number(habitacaoPorMes) + Number(internetPorMes) + Number(televisaoPorMes) + Number(telefonePorMes) + Number(saudePorMes) + Number(impostoPorMes) + Number(veiculoPorMes) + Number(despesasPessoaisPorMes) + Number(lazerPorMes); 
-    let lucroMensal = Number(rendaPorMes) - Number(somaDosGastosMensal);
-
+    const somaDosGastosMensal = () => Number(aguaPorMes) + Number(luzPorMes) + Number(mercadoPorMes) + Number(habitacaoPorMes) + Number(internetPorMes) + Number(televisaoPorMes) + Number(telefonePorMes) + Number(saudePorMes) + Number(impostoPorMes) + Number(veiculoPorMes) + Number(despesasPessoaisPorMes) + Number(lazerPorMes); 
+    const lucroMensal = () => Number(rendaPorMes) - somaDosGastosMensal();
+    
     // Resultados Mensal: Lucro e Soma dos Gastos
-    function resultadoLucroMensal(){
+    (function resultadoLucroMensal(){
         let resultadoMensal = document.querySelector("#saldoDoMes");
-        if(lucroMensal > 0 ){
-            resultadoMensal.innerText = `Seu mês fecha ANIMADO com um SALDO POSITIVO de  ${lucroMensal} reais.`;
-        } 
-        else if(lucroMensal === 0){
-            resultadoMensal.innerText = `Seu mês fecha ZERADO!`;
-        }
-        else{
-            resultadoMensal.innerText = `Seu mês fecha TRISTE com um SALDO NEGATIVO de  ${lucroMensal} reais.`;
-        }
-    } resultadoLucroMensal();
+        if(lucroMensal() > 0 ) resultadoMensal.innerText = `Seu mês fecha ANIMADO com um SALDO POSITIVO de  ${lucroMensal()} reais.`;
+        else if(lucroMensal() === 0) resultadoMensal.innerText = `Seu mês fecha ZERADO!`;
+        else resultadoMensal.innerText = `Seu mês fecha TRISTE com um SALDO NEGATIVO de  ${lucroMensal()} reais.`;
+    } ());
 
-    function resultadoSomaMensal(){
+    (function resultadoSomaMensal(){
         let somaMensal = document.querySelector("#somaDosGastosDoMes");
-            somaMensal.innerText = `O seu gasto mensal é de ${somaDosGastosMensal} reais.`;
-            if(somaDosGastosMensal === 0){
-                somaMensal.innerText = `O seu gasto mensal está ZERADO!`;
-            }
-    } resultadoSomaMensal();
+        somaMensal.innerText = `O seu gasto mensal é de ${somaDosGastosMensal()} reais.`;
+        if(somaDosGastosMensal() === 0) somaMensal.innerText = `O seu gasto mensal está ZERADO!`;
+    } ());
     
+
+    const mediaAnualPorCategoria = (categoria) => categoria * 12;
+    
+
     //Gastos anual separados em categorias:
-    function aguaTotal(){
-        let aguaAnual = Number(aguaPorMes) * 12;
+    (function aguaTotal(){
+        let aguaAnual = mediaAnualPorCategoria(aguaPorMes);
         let textoAguaAnual = document.querySelector("#aguaPorAno");
-            textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: R$ ${aguaAnual}`;    
-            if(aguaAnual === 0){
-                textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: sem gasto ou não preenchido.`;
-            }       
-    } aguaTotal();
+        textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: R$ ${aguaAnual}`;    
+        if(aguaAnual === 0) textoAguaAnual.innerHTML = `🌊 Gasto com <b>ÁGUA</b>: sem gasto ou não preenchido.`;      
+    } ());
         
-    function luzTotal(){
-        let luzAnual = Number(luzPorMes) * 12;
+    (function luzTotal(){
+        let luzAnual = mediaAnualPorCategoria(luzPorMes);
         let textoLuzAnual = document.querySelector("#luzPorAno");
-            textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: R$ ${luzAnual}`;
-            if(luzAnual === 0){
-                textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: sem gasto ou não preenchido.`;
-            }     
-    } luzTotal();
+        textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: R$ ${luzAnual}`;
+        if(luzAnual === 0) textoLuzAnual.innerHTML = `⚡ Gasto com <b>ENERGIA</b>: sem gasto ou não preenchido.`;
+    } ());
 
-    function mercadoTotal(){
-        let mercadoAnual = Number(mercadoPorMes) * 12;
+    (function mercadoTotal(){
+        let mercadoAnual = mediaAnualPorCategoria(mercadoPorMes);
         let textoMercadoAnual = document.querySelector("#mercadoPorAno");
-            textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: R$ ${mercadoAnual}`;
-            if(mercadoAnual === 0){ 
-                textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: sem gasto ou não preenchido.`;
-            }     
-    } mercadoTotal();
+        textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: R$ ${mercadoAnual}`;
+        if(mercadoAnual === 0) textoMercadoAnual.innerHTML = `🏪 Gasto com <b>MERCADO</b>: sem gasto ou não preenchido.`;
+    } ());
 
-    function habitacaoTotal(){
-        let habitacaoAnual = Number(habitacaoPorMes) * 12;
+    (function habitacaoTotal(){
+        let habitacaoAnual = mediaAnualPorCategoria(habitacaoPorMes);
         let textoHabitacaoAnual = document.querySelector("#habitacaoPorAno");
-            textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: R$ ${habitacaoAnual}`;
-            if(habitacaoAnual === 0){
-                textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: sem gasto ou não preenchido.`;
-            }     
-    } habitacaoTotal();
+        textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: R$ ${habitacaoAnual}`;
+        if(habitacaoAnual === 0) textoHabitacaoAnual.innerHTML = `🏠 Gasto com <b>HABITAÇÃO</b>: sem gasto ou não preenchido.`;
+    } ());
     
-    function internetTotal(){
-        let internetAnual = Number(internetPorMes) * 12;
+    (function internetTotal(){
+        let internetAnual = mediaAnualPorCategoria(internetPorMes);
         let textoInternetAnual = document.querySelector("#internetPorAno");
-            textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: R$ ${internetAnual}`;
-            if(internetAnual === 0){
-                textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: sem gasto ou não preenchido.`;
-            } 
-    } internetTotal();   
+        textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: R$ ${internetAnual}`;
+        if(internetAnual === 0) textoInternetAnual.innerHTML = `🌐 Gasto com <b>INTERNET</b>: sem gasto ou não preenchido.`;
+    } ());   
 
-    function televisaoTotal(){
-        let televisaoAnual = Number(televisaoPorMes) * 12;
+    (function televisaoTotal(){
+        let televisaoAnual = mediaAnualPorCategoria(televisaoPorMes);
         let textoTelevisaoAnual = document.querySelector("#televisaoPorAno");
-            textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: R$ ${televisaoAnual}`;
-            if(televisaoAnual === 0){
-                textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: sem gasto ou não preenchido.`;
-            }
-    } televisaoTotal();
+        textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: R$ ${televisaoAnual}`;
+        if(televisaoAnual === 0) textoTelevisaoAnual.innerHTML = `📺 Gasto com <b>TV</b>: sem gasto ou não preenchido.`;
+    } ());
 
-    function telefoneTotal(){
-        let telefoneAnual = Number(telefonePorMes) * 12;
+    (function telefoneTotal(){
+        let telefoneAnual = mediaAnualPorCategoria(telefonePorMes);
         let textoTelefoneAnual = document.querySelector("#telefonePorAno");
-            textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: R$ ${telefoneAnual}`;
-            if(telefoneAnual === 0){
-                textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: sem gasto ou não preenchido.`;
-            }
-    } telefoneTotal();
+        textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: R$ ${telefoneAnual}`;
+        if(telefoneAnual === 0) textoTelefoneAnual.innerHTML = `☎️ Gasto com <b>TELEFONE</b>: sem gasto ou não preenchido.`;
+    } ());
 
-    function saudeTotal(){
-        let saudeAnual = Number(saudePorMes) * 12;
+    (function saudeTotal(){
+        let saudeAnual = mediaAnualPorCategoria(saudePorMes);
         let textoSaudeAnual = document.querySelector("#saudePorAno");
-            textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: R$ ${saudeAnual}`;
-            if(saudeAnual === 0){
-                textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: sem gasto ou não preenchido.`;
-            }     
-    } saudeTotal();
+        textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: R$ ${saudeAnual}`;
+        if(saudeAnual === 0) textoSaudeAnual.innerHTML = `🩺 Gasto com <b>SAÚDE</b>: sem gasto ou não preenchido.`;
+    } ());
 
-    function impostoTotal(){
-        let impostoAnual = Number(impostoPorMes) * 12;
+    (function impostoTotal(){
+        let impostoAnual = mediaAnualPorCategoria(impostoPorMes);
         let textoImpostoAnual = document.querySelector("#impostoPorAno");
-            textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: R$ ${impostoAnual}`;
-            if(impostoAnual === 0){
-                textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: sem gasto ou não preenchido.`;
-            }     
-    } impostoTotal();
+        textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: R$ ${impostoAnual}`;
+        if(impostoAnual === 0) textoImpostoAnual.innerHTML = `💰 Gasto com <b>IMPOSTO</b>: sem gasto ou não preenchido.`;    
+    } ());
 
-    function veiculoTotal(){
-        let veiculoAnual = Number(veiculoPorMes) * 12;
+    (function veiculoTotal(){
+        let veiculoAnual = mediaAnualPorCategoria(veiculoPorMes);
         let textoVeiculoAnual = document.querySelector("#veiculoPorAno");
-            textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: R$ ${veiculoAnual}`;
-            if(veiculoAnual === 0){
-                textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: sem gasto ou não preenchido.`;
-            }     
-    } veiculoTotal();
+        textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: R$ ${veiculoAnual}`;
+        if(veiculoAnual === 0) textoVeiculoAnual.innerHTML = `🚗 Gasto com <b>AUTOMÓVEL</b>: sem gasto ou não preenchido.`;     
+    } ());
 
-    function despesasPessoaisTotal(){
-        let despesasPessoaisAnual = Number(despesasPessoaisPorMes) * 12;
+    (function despesasPessoaisTotal(){
+        let despesasPessoaisAnual = mediaAnualPorCategoria(despesasPessoaisPorMes);
         let textoDespesasAnual = document.querySelector("#despesasPessoaisPorAno");
-            textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: R$ ${despesasPessoaisAnual}`;
-            if(despesasPessoaisAnual === 0){
-                textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: sem gasto ou não preenchido.`;
-            }     
-    } despesasPessoaisTotal();
+        textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: R$ ${despesasPessoaisAnual}`;
+        if(despesasPessoaisAnual === 0) textoDespesasAnual.innerHTML = `💄 Gasto com <b>DESPESAS PESSOAIS</b>: sem gasto ou não preenchido.`;   
+    } ());
 
-    function lazerTotal(){
-        let lazerAnual = Number(lazerPorMes) * 12;
+    (function lazerTotal(){
+        let lazerAnual = mediaAnualPorCategoria(lazerPorMes);
         let textoLazerAnual = document.querySelector("#lazerPorAno");
-            textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: R$ ${lazerAnual}`;
-            if(lazerAnual == 0){
-                textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: sem gasto ou não preenchido.`;
-            }
-    } lazerTotal();
+        textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: R$ ${lazerAnual}`;
+        if(lazerAnual == 0) textoLazerAnual.innerHTML = `🍔 Gasto com <b>LAZER</b>: sem gasto ou não preenchido.`;
+    } ());
 
     // Dados Finais: Renda anual, gastos anual total e lucro/perda anual.
-    let rendaAnual = Number(rendaPorMes) * 12;
-    function rendaPorAno(){ 
+    let rendaAnual = mediaAnualPorCategoria(rendaPorMes);
+    (function rendaPorAno(){ 
         let textoRendaAnual = document.querySelector("#rendaAnual");
-            textoRendaAnual.innerHTML = `Sua <b>RENDA</b> anual gira em torno de R$ ${rendaAnual}`;
-            if(rendaAnual > 0){
-                textoRendaAnual.style.color = "#0C9FCA";
-            }
-            else{
-                textoRendaAnual.innerText = `Renda anual: [ERRO] Preencha os dados para ver o resultado!`;
-                textoLucroAnual.style.fontColor = "#F00D25";
-            }
-    } rendaPorAno();
+        textoRendaAnual.innerHTML = `Sua <b>RENDA</b> anual gira em torno de R$ ${rendaAnual}`;
+        if(rendaAnual > 0) textoRendaAnual.style.color = "#0C9FCA";
+        else{
+            textoRendaAnual.innerText = `Renda anual: [ERRO] Preencha os dados para ver o resultado!`;
+            textoRendaAnual.style.fontColor = "#F00D25";
+        }
+    } ());
 
-    let somaDosGastosAnual = Number(somaDosGastosMensal) * 12;
-    function gastoPorAno(){
+    let somaDosGastosAnual = mediaAnualPorCategoria(somaDosGastosMensal());
+    (function gastoPorAno(){
         let textoSomaAnual = document.querySelector("#somaDosGastosAnual");
             textoSomaAnual.innerHTML = `Seu <b>GASTO</b> anual gira em torno de R$ ${somaDosGastosAnual}`;
             textoSomaAnual.style.color = "#D80D22"; 
-            if(somaDosGastosAnual === 0){
-                textoSomaAnual.innerText = `Gasto anual: [ERRO] Preencha os dados para ver o resultado!`;
-            }
-    } gastoPorAno();
+            if(somaDosGastosAnual === 0) textoSomaAnual.innerText = `Gasto anual: [ERRO] Preencha os dados para ver o resultado!`;
+    } ());
 
-    function lucroAoAno(){
+    (function lucroAoAno(){
         let lucroAnual = rendaAnual - somaDosGastosAnual;
         let textoLucroAnual = document.querySelector("#lucroAnual");
             textoLucroAnual.innerHTML = `Seu <b>LUCRO</b> anual gira em torno de R$ ${lucroAnual}`;
-            if(lucroAnual > 0){
-                textoLucroAnual.style.color = "#19D327";
-            }
+            if(lucroAnual > 0) textoLucroAnual.style.color = "#19D327";
             else if(lucroAnual === 0){
                 textoLucroAnual.innerHTML = `Seu <b>LUCRO</b> anual é IGUAL a 0 ou você NÃO preencheu sua renda ou seus gastos`;
                 textoLucroAnual.style.color = "#0DBAEC";
@@ -189,10 +158,9 @@ function resultados(){
                 textoLucroAnual.innerHTML = `Sua <b>PERDA</b> anual gira em torno de R$ ${lucroAnual}`;
                 textoLucroAnual.style.color = "#D80D22";
             }
-    } lucroAoAno();
+    } ());
 }
 
 function misterio(){
-    alert(`Parece que você encontrou meu easter egg!!!
-Na verdade não é nada demais kkk só quero te desejar um bom dia e que tudo na sua vida dê certo 😊`);
+    alert(`Parece que você encontrou meu easter egg!!!`);
 }
